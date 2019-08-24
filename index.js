42e9e363e041b093f51d9592cabec088e18679a6
rifmap <medghost0@gmail.com>
2019-08-24 16:24:21 +0000

    make it better.!.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const user = require('./routes/user');
 
 
 app.use(express.static('uploads'));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 });
 app.use(bodyParser.json());
@@ -42,7 +42,7 @@ app.use(session({
 app.use('/api/shops', shops);
 app.use('/api/user', user);
 
-// app.use(error);
+app.use(error);
 
 app.listen(process.env.PORT || 5000, () =>{
     console.log('app running on port : 5000');
