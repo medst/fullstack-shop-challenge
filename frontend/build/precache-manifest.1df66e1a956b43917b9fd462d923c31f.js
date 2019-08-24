3816ca374c776a1bb07f4dc9520bbf4eb7a3c782
rifmap <medghost0@gmail.com>
2019-08-24 16:18:10 +0000

    make it better..!

---
+++ frontend/build/precache-manifest.1df66e1a956b43917b9fd462d923c31f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48796459d3d43b582c59dca2f1e67bc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec75f517e197b38e74ef",
+    "url": "/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "a1ddbba518c218d6d728",
+    "url": "/static/css/main.8b056581.chunk.css"
+  },
+  {
+    "revision": "ec75f517e197b38e74ef",
+    "url": "/static/js/2.77972236.chunk.js"
+  },
+  {
+    "revision": "a1ddbba518c218d6d728",
+    "url": "/static/js/main.17bcc988.chunk.js"
+  },
+  {
+    "revision": "219707186523ad4b8cde",
+    "url": "/static/js/runtime~main.821e3928.js"
+  }
+]);
