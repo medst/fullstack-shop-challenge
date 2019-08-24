2415597bf89370ae7bb91054f28d6bbfb41cfef4
rifmap <medghost0@gmail.com>
2019-08-24 14:59:15 +0000

    app work fine

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const bodyParser = require('body-parser');
 
-const pdb = "mongodb+srv://medst:mohanader@cluster0-vwt5h.mongodb.net/test?retryWrites=true&w=majority";
+const pdb = "your db path";
 
 mongoose.connect(pdb, { useNewUrlParser: true });
 require('./models/shops');
